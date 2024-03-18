a1006d321619f5111f8789222971057adc860823
Sayantan Sengupta <56554981+iam-Sayantan@users.noreply.github.com>
2024-03-18 10:49:07 +0530

    Constructors added

---
+++ src/OOPs/Constructors/Animal.java
@@ -0,0 +1,41 @@
+package OOPs.Constructors;
+
+public class Animal
+{
+    int age;
+    String color;
+
+    public Animal(int age1, String color1)
+    {
+        age=age1;
+        color=color1;
+    }
+
+    void eat()
+    {
+        System.out.println("I am eating");
+    }
+
+    void run()
+    {
+        System.out.println("I am running");
+    }
+
+    public static void main(String[] args)
+    {
+        Animal tiger = new Animal(10, "red");
+        System.out.println(tiger.age);
+        System.out.println(tiger.color);
+        tiger.eat();
+        tiger.run();
+
+        System.out.println("=================================");
+
+        Animal lion = new Animal(23,"yellow");
+        System.out.println(lion.age);
+        System.out.println(lion.color);
+        lion.eat();
+        lion.run();
+    }
+
+}
