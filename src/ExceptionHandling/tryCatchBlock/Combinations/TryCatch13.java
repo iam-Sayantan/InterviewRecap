092a036ad0be2fecfc6c92f829a6aa1db79bbc9c
Sayantan Sengupta <56554981+iam-Sayantan@users.noreply.github.com>
2024-05-04 11:54:34 +0530

    Added till exceptions

--- src/ExceptionHandling/tryCatchBlock/Combinations/TryCatch13.java
+++ src/ExceptionHandling/tryCatchBlock/Combinations/TryCatch13.java
@@ -10,10 +10,23 @@ public class TryCatch13
             System.out.println("2");
             System.out.println(100/0);
         }
+        /*
+
+        This is possible only since exception class is before Throwable class.
+        Exception class is the child class of the Throwable class. So
+        this means we always need to put child classes before parent classes in case of
+        multiple exceptions in catch blocks
+
+         */
+
+        // This catch block catches exceptions of type Exception and its subclasses.
+        // Since Exception is a child class of Throwable, it must come before the catch block for Throwable.
         catch (Exception e)
         {
             System.out.println("3");
         }
+        // This catch block catches exceptions of type Throwable and its subclasses.
+       // However, it won't catch exceptions that have already been caught by a preceding catch block for a more specific subclass of Throwable.
         catch (Throwable e)
         {
             System.out.println("4");
